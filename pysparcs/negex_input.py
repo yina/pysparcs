b2386cf272bc9ed539511fd0a7ef1a24ff7ce4d3
ylteq <liangyu.teq@gmail.com>
2015-01-28 01:39:01 -0500

    Negex_input.py

---
+++ pysparcs/negex_input.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import re
+import sys
+import click
+
+@click.command()
+@click.option('--f1', help='extracted concepts file from cTakes without extension')
+@click.option('--f2', help='medical note file without extension')
+@click.option('--outdir', help='output directory')
+
+
+def negex_input(f1,f2,outdir):
+    file1 = open(f1+".txt","r")
+    file2 = open(f2+".txt","r")
+    outfile = open(outdir+"/"+f2+'_negexinput.txt', 'w')
+    logfile = open(outdir+"/"+f2+'_log.txt','w')
+    outfile.write('Report No.'+'\t'+'Concept'+'\t'+'\t'+'Sentence'+'\n')
+
+
+    concepts = file1.readlines()
+    mynote = file2.readlines()
+    mynote_new = filter(lambda x: x not in '\n',mynote)
+    for i in range(len(concepts)):
+        tmp = concepts[i].rstrip()
+#        print tmp
+        for lines in mynote_new: 
+            str_split = re.split(r"\t| {2,9}|\.|\;",lines)
+            for s in str_split:
+                mymatch = re.findall(tmp,s)
+                if len(mymatch)>0:
+                    outfile.write('1'+'\t'+tmp+'\t'+"\""+s+"\""+'\n')
+                    if len(re.findall(tmp,s)) > 1:
+                        logfile.write(tmp+":Mutiple occurence in:"+s+"\n")
+                        
+if __name__ == '__main__':
+    negex_input()
+    
+        
+                
+            
+
+
+
+
+
+
+
+				
+
+
